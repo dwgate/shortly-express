0655fcafbad5af21c29d5b697d295feb69deb3cc
Hack Reactor Student <student@hackreactor.com>
2017-04-24 20:16:57 -0700

    implemented sessions schema

--- server/db/config.js
+++ server/db/config.js
@@ -43,7 +43,11 @@ module.exports = (db) => {
     .then(() => {
       return db.queryAsync(`
         CREATE TABLE IF NOT EXISTS sessions (
-        `);
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        hash CHAR(64),
+        user_id INT,
+        timestamp TIMESTAMP
+        );`);
     })
 
     .error(err => {
